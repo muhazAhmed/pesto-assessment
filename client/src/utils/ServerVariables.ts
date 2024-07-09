30580bec2cc47897707a83707e4933d1b2aeb404
Muhaz Ahmed <muhazvla313@gmail.com>
2024-07-09 21:13:44 +0530

    added new server variables

--- client/src/utils/ServerVariables.ts
+++ client/src/utils/ServerVariables.ts
@@ -1,3 +1,12 @@
 export const serverVariables = {
     RELOAD_SERVER: "/load",
+    
+    NEW_USER: "/user/new",
+    FETCH_ONE_USER: "/user/fetch/",
+
+    NEW_TASK: "/tasks/new/",
+    FETCH_ONE_TASK: "/tasks/fetch/",
+    FETCH_ALL_TASK: "/tasks/fetch-all/",
+    UPDATE_TASK: "/tasks/update/",
+    DELETE_TASK: "/tasks/delete/",
 }
