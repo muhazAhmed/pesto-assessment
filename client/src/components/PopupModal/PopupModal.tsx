f54622d7ae7cf9ef5e80735d7182d91b20d55079
Muhaz Ahmed <muhazvla313@gmail.com>
2024-07-09 21:12:45 +0530

    fixed popup modal bugs

--- client/src/components/PopupModal/PopupModal.tsx
+++ client/src/components/PopupModal/PopupModal.tsx
@@ -16,7 +16,7 @@ const PopupModal: FC<ModalProps> = ({ setModal, children, className }) => {
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
       exit={{ opacity: 0 }}
-      transition={{ duration: 0.3 }}
+      transition={{ duration: 0.5 }}
     >
       <div className="modal-content">
         <i
