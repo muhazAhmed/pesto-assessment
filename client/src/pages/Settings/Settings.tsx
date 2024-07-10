0734a36c7cadb63f4738e4b7a7593f8ac0d2f162
Muhaz Ahmed <muhazvla313@gmail.com>
2024-07-10 22:32:40 +0530

    bugs fix

--- client/src/pages/Settings/Settings.tsx
+++ client/src/pages/Settings/Settings.tsx
@@ -2,9 +2,17 @@ import "./style.scss";
 import Switch from "react-switch";
 import { toggleItems } from "./ArrayOfItems";
 import { useSettings } from "../../utils/SettingsContext";
+import { useState } from "react";
+import DeleteModal from "../Dashboard/SubComponents/DeleteModal";
+import { fetchUserId, openModal } from "../../utils/commonFunctions";
+import Login from "../Login/Login";
+import Loading from "../../components/Loading/Loading";
 
 const Settings = () => {
   const { settings, updateSettings, resetSettings } = useSettings();
+  const [logoutModal, setLogoutModal] = useState<boolean>(false);
+  const [formModal, setFormModal] = useState<boolean>(false);
+  const [loading, setLoading] = useState<boolean>(false);
 
   const handleToggleAction = (item: string) => {
     const updatedSettings = { ...settings, [item]: !settings[item] };
@@ -13,6 +21,9 @@ const Settings = () => {
 
   return (
     <div className="settings">
+      {loading && <Loading />}
+      {logoutModal && <DeleteModal setModal={setLogoutModal} page="logout" />}
+      {formModal && <Login setLoading={setLoading} setModal={formModal} />}
       <h1>Settings</h1>
       <div className="setting-container">
         <h3>General Settings</h3>
@@ -31,6 +42,20 @@ const Settings = () => {
           </div>
         ))}
         <button onClick={resetSettings}>Reset to Default</button>
+      </div>
+      <div className="setting-container">
+        <h3>Account Settings</h3>
+        <div className="items">
+          <label>{fetchUserId ? "Switch account / Logout" : "Login"}</label>
+          <button
+            id={fetchUserId ? "logout" : "login"}
+            onClick={() =>
+              openModal(fetchUserId ? setLogoutModal : setFormModal)
+            }
+          >
+            {fetchUserId ? "Logout" : "Login"}
+          </button>
+        </div>
       </div>
     </div>
   );
