df65ae37cd1e6943e9672fb25d367d7a99a9a9b7
Muhaz Ahmed <muhazvla313@gmail.com>
2024-07-10 22:30:51 +0530

    conditionally rendering modal

--- client/src/pages/Dashboard/SubComponents/DeleteModal.tsx
+++ client/src/pages/Dashboard/SubComponents/DeleteModal.tsx
@@ -1,22 +1,44 @@
 import "./style.scss";
 import PopupModal from "../../../components/PopupModal/PopupModal";
 import { FC } from "react";
-import { closeModal } from "../../../utils/commonFunctions";
+import { closeModal, logout } from "../../../utils/commonFunctions";
+import { useNavigate } from "react-router-dom";
 
 interface PopupModalProps {
   setModal: any;
-  userInfo?: any;
+  taskInfo?: any;
+  page: string;
 }
 
-const DeleteModal: FC<PopupModalProps> = ({ setModal, userInfo }) => {
+const DeleteModal: FC<PopupModalProps> = ({ setModal, taskInfo, page }) => {
+  const navigate = useNavigate();
+
+  const handleButton = async () => {
+    if (page == "logout") {
+      return logout(navigate);
+    } else {
+      // ====
+    }
+  };
+
   return (
     <PopupModal setModal={setModal}>
       <div className="delete-modal">
         <h3>
-          Are Sure want to <span>Delete</span> this item
+          Are you sure you want to
+          {page === "delete" ? (
+            <>
+              <span>Delete</span> this item
+            </>
+          ) : (
+            <span> logout</span>
+          )}
+          ?
         </h3>
         <div className="buttons">
-          <button onClick={() => console.log(userInfo)}>Delete</button>
+          <button onClick={handleButton}>
+            {page == "delete" ? "Delete" : "Logout"}
+          </button>
           <button onClick={() => closeModal(setModal)}>Cancel</button>
         </div>
       </div>
