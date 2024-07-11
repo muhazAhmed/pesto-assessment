f4c5674863d2a93b621b78cf8a81fc1a77490834
Muhaz Ahmed <muhazvla313@gmail.com>
2024-07-11 20:41:32 +0530

    dummy data task

---
+++ client/src/pages/DummyData.ts
@@ -0,0 +1,26 @@
+export const taskDummyItems = [
+    {
+      date: "12-04-2024",
+      title: "demo title",
+      description: "ns properties like Padding, Spacing, Spacing Mode",
+      status: "Done",
+    },
+    {
+      date: "10-04-2024",
+      title: "demo title 2",
+      description: "ns properties like Padding, Spacing, Spacing Mode",
+      status: "In Progress",
+    },
+    {
+      date: "10-04-2024",
+      title: "demo title 2",
+      description: "ns properties like Padding, Spacing, Spacing Mode",
+      status: "In Progress",
+    },
+    {
+      date: "10-04-2024",
+      title: "demo title 2",
+      description: "ns properties like Padding, Spacing, Spacing Mode",
+      status: "In Progress",
+    },
+  ];
