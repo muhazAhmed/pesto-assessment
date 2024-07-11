32519c5b03ce5827fae08e0df3adc9bbc722f8d6
Muhaz Ahmed <muhazvla313@gmail.com>
2024-07-11 20:41:24 +0530

    login modal popup

--- client/src/components/sidebar/Sidebar.tsx
+++ client/src/components/sidebar/Sidebar.tsx
@@ -6,9 +6,16 @@ import toast from "react-hot-toast";
 import { useSettings } from "../../utils/SettingsContext";
 import { ResponseMessage } from "../../utils/Validations";
 import EditModal from "../../pages/Dashboard/SubComponents/EditModal";
-import { fetchUserId, openModal } from "../../utils/commonFunctions";
+import {
+  fetchUserId,
+  newLocalStorage,
+  openModal,
+  useLocalStorage,
+} from "../../utils/commonFunctions";
 import Login from "../../pages/Login/Login";
 import Loading from "../Loading/Loading";
+import { fetchAllTasks } from "../../utils/onPageLoad";
+import WarningPopup from "../../pages/Dashboard/SubComponents/WarningPopup";
 
 interface SidebarProps {
   children: any;
@@ -18,6 +25,7 @@ const Sidebar: FC<SidebarProps> = ({ children }) => {
   const [addModal, setAddModal] = useState<boolean>(false);
   const [loading, setLoading] = useState<boolean>(false);
   const [formModal, setFormModal] = useState<boolean>(false);
+  const [warningModal, setWarningModal] = useState<boolean>(false);
   const navigate = useNavigate();
   const { settings } = useSettings();
 
@@ -25,6 +33,21 @@ const Sidebar: FC<SidebarProps> = ({ children }) => {
     const currentPath = location.pathname;
     setSelectedItem(pathToIndex[currentPath] ?? null);
   }, [location.pathname]);
+
+  useEffect(() => {
+    const warning = useLocalStorage("1rfg3");
+    const warningRead = useLocalStorage("dfd433");
+    setTimeout(() => {
+      if (warning === false && !fetchUserId) {
+        if (!warningRead) {
+          newLocalStorage("1rfg3", true);
+          setWarningModal(true);
+        } else return;
+      } else {
+        newLocalStorage("1rfg3", false);
+      }
+    }, 7000);
+  }, []);
 
   const handleClick = (index: number, path: string) => {
     setSelectedItem(index);
@@ -36,7 +59,7 @@ const Sidebar: FC<SidebarProps> = ({ children }) => {
       if (fetchUserId) {
         return openModal(setAddModal);
       } else {
-        toast.error("Please Login")
+        toast.error("Please Login");
         return openModal(setFormModal);
       }
     } else {
@@ -52,11 +75,21 @@ const Sidebar: FC<SidebarProps> = ({ children }) => {
   return (
     <div className="app-container">
       {loading && <Loading />}
-      {addModal && <EditModal setShowModal={setAddModal} page="add" />}
+      {warningModal && (
+        <WarningPopup setModal={setWarningModal} setFormModal={setFormModal} />
+      )}
+      {addModal && (
+        <EditModal
+          setShowModal={setAddModal}
+          page="add"
+          setLoading={setLoading}
+          fetchData={() => fetchAllTasks(setLoading)}
+        />
+      )}
       {formModal && <Login setModal={setFormModal} setLoading={setLoading} />}
       <div className="sidebar">
         <div className="sidebar-container">
-          <h1>Logo</h1>
+          <h1 onClick={() => navigate("/")}>Logo</h1>
           {sidebarItems?.map((item: any, index: number) => (
             <div
               className={
