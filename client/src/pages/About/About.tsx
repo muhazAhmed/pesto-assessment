1daff3ce94137c3ff4d32ba405fac25febcd649d
Muhaz Ahmed <muhazvla313@gmail.com>
2024-07-12 16:59:29 +0530

    added about us page

--- client/src/pages/About/About.tsx
+++ client/src/pages/About/About.tsx
@@ -1,9 +1,58 @@
-import "./style.scss"
+import { directWithNewTab, useLocalStorage } from "../../utils/commonFunctions";
+import { motion } from "framer-motion";
+import "./style.scss";
 
 const About = () => {
-  return (
-    <div>About</div>
-  )
-}
+  const defaultSettings = useLocalStorage("defaultSettings");
 
-export default About
+  return (
+    <div className="about">
+      <h1>About</h1>
+      <div className="card">
+        <motion.div
+          className="item"
+          onClick={() => directWithNewTab("https://github.com/muhazAhmed")}
+          initial={
+            defaultSettings?.isAnimationEnabled && { scale: 0.8, opacity: 0 }
+          }
+          animate={{ scale: 1, opacity: 1 }}
+          transition={{ duration: 0.5 }}
+          whileHover={defaultSettings?.isAnimationEnabled && { scale: 1.1 }}
+        >
+          <i className="fa-brands fa-github"></i>
+          <p>Check out my GitHub repositories and projects.</p>
+        </motion.div>
+        <motion.div
+          className="item"
+          onClick={() =>
+            directWithNewTab("https://linkedin.com/in/muhazahmed/")
+          }
+          initial={
+            defaultSettings?.isAnimationEnabled && { scale: 0.8, opacity: 0 }
+          }
+          animate={{ scale: 1, opacity: 1 }}
+          transition={{ duration: 0.3 }}
+          whileHover={defaultSettings?.isAnimationEnabled && { scale: 1.1 }}
+        >
+          <i className="fa-brands fa-linkedin"></i>
+          <p>Connect with me on LinkedIn for professional networking.</p>
+        </motion.div>
+        <motion.div
+          className="item"
+          onClick={() => directWithNewTab("https://instagram.com/muhaz_ahmd")}
+          initial={
+            defaultSettings?.isAnimationEnabled && { scale: 0.8, opacity: 0 }
+          }
+          animate={{ scale: 1, opacity: 1 }}
+          transition={{ duration: 0.3 }}
+          whileHover={defaultSettings?.isAnimationEnabled && { scale: 1.1 }}
+        >
+          <i className="fa-brands fa-instagram"></i>
+          <p>Follow me on Instagram to see my latest photos and updates.</p>
+        </motion.div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
